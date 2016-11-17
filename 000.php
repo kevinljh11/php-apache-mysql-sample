8bc642394ac83712877ceed0b0367e0054e3ff1c
kevinljh11 <kevinljh1@hotmail.com>
2016-11-18 01:10:29 +0800

    Update 000.php

--- 000.php
+++ 000.php
@@ -3,8 +3,11 @@ ini_set('user_agent','Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.22 (KHTML, li
 error_reporting(E_ALL ^ E_NOTICE);
 ini_set('date.timezone','Asia/Shanghai'); 
 
-$url1="http://live.dszuqiu.com/";
-
-$data1=file_get_contents($url1);
-echo $data1;
+$url="http://live.dszuqiu.com/";
+$ch = curl_init(); 
+curl_setopt ($ch, CURLOPT_URL, $url); 
+curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1); 
+curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT,10); 
+$data = curl_exec($ch); 
+echo $data;
 ?>?
